68c2cf1dd36be0f2ab9d917cb49572c7ad6a0129
samee2612 <sameeksha2612@gmail.com>
2021-10-13 09:51:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vision/objectDetectorClass.java
@@ -0,0 +1,178 @@
+package com.example.vision;
+
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.gpu.GpuDelegate;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class objectDetectorClass {
+
+    //this is used to load model and predict
+    private Interpreter interpreter;
+    //show all label in list
+    private List<String> labelList;
+    private int INPUT_SIZE;
+    private int PIXEL_SIZE = 3; //for RGB
+    private int IMAGE_MEAN = 0;
+    private float IMAGE_STD = 255.0f;
+    //use to initialize gpu in app
+    private GpuDelegate gpuDelegate;
+    private int height = 0;
+    private int width = 0;
+
+    objectDetectorClass(AssetManager assetManager, String modelPath, String labelPath, int inputSize) throws IOException {
+        INPUT_SIZE = inputSize;
+        //use to define gpu or cpu // no. of threads
+        Interpreter.Options options = new Interpreter.Options();
+        gpuDelegate = new GpuDelegate();
+        options.addDelegate(gpuDelegate);
+        options.setNumThreads(4); //set acc to phone
+        //load model
+        interpreter = new Interpreter(loadModelFile(assetManager, modelPath), options);
+        //load labelmap
+        labelList = loadLabelList(assetManager, labelPath);
+
+    }
+
+    private List<String> loadLabelList(AssetManager assetManager, String labelPath) throws IOException {
+        //to store label
+        List<String> labelList = new ArrayList<>();
+        //create a new reader
+        BufferedReader reader = new BufferedReader(new InputStreamReader(assetManager.open(labelPath)));
+        String line;
+        //loop through each line and store it to labelList
+        while ((line = reader.readLine()) != null) {
+            labelList.add(line);
+        }
+        reader.close();
+        return labelList;
+    }
+
+
+    private ByteBuffer loadModelFile(AssetManager assetManager, String modelPath) throws IOException {
+        // use to get description of file
+        AssetFileDescriptor fileDescriptor = assetManager.openFd(modelPath);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+
+    //New Mat function
+    public Mat recognizeImage(Mat mat_image) {
+        // Rotate original image by 90 degree get get portrait frame
+        Mat rotated_mat_image = new Mat();
+        Mat a=mat_image.t();
+        Core.flip(a,mat_image,1);
+        a.release();
+        // if you do not do this process you will get improper prediction, less no. of object
+        // now convert it to bitmap
+        Bitmap bitmap=null;
+        bitmap=Bitmap.createBitmap(rotated_mat_image.cols(),rotated_mat_image.rows(),Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(rotated_mat_image,bitmap);
+        // define height and width
+        height=bitmap.getHeight();
+        width=bitmap.getWidth();
+
+        // scale the bitmap to input size of model
+        Bitmap scaledBitmap=Bitmap.createScaledBitmap(bitmap,INPUT_SIZE,INPUT_SIZE,false);
+
+        // convert bitmap to bytebuffer as model input should be in it
+        ByteBuffer byteBuffer=convertBitmapToByteBuffer(scaledBitmap);
+
+        // defining output
+        // 10: top 10 object detected
+        // 4: there coordinate in image
+        Object[] input=new Object[1];
+        input[0]=byteBuffer;
+
+        Map<Integer,Object> output_map=new TreeMap<>();
+
+        //Create treemap of three array (boxes,score,classes)
+        float[][][]boxes =new float[1][10][4];
+
+        // 10: top 10 object detected
+        // 4: there coordinate in image
+        float[][] scores=new float[1][10];
+
+        // stores scores of 10 object
+        float[][] classes=new float[1][10];
+        // stores class of object
+
+        // add it to object_map;
+        output_map.put(0,boxes);
+        output_map.put(1,classes);
+        output_map.put(2,scores);
+
+        // now predict
+        interpreter.runForMultipleInputsOutputs(input,output_map);
+
+
+        // before returning rotate back by -90 degree
+        Mat b=mat_image.t();
+        Core.flip(b,mat_image,0);
+        b.release();
+
+        return mat_image;
+
+    }
+
+    private ByteBuffer convertBitmapToByteBuffer(Bitmap bitmap) {
+        ByteBuffer byteBuffer;
+        // some model input should be quant=0  for some quant=1
+        // for this quant=0
+
+        int quant=0;
+        int size_images=INPUT_SIZE;
+        if(quant==0){
+            byteBuffer=ByteBuffer.allocateDirect(1*size_images*size_images*3);
+        }
+        else {
+            byteBuffer=ByteBuffer.allocateDirect(4*1*size_images*size_images*3);
+        }
+        byteBuffer.order(ByteOrder.nativeOrder());
+        int[] intValues=new int[size_images*size_images];
+        bitmap.getPixels(intValues,0,bitmap.getWidth(),0,0,bitmap.getWidth(),bitmap.getHeight());
+        int pixel=0;
+
+
+        for (int i=0;i<size_images;++i){
+            for (int j=0;j<size_images;++j){
+                final  int val=intValues[pixel++];
+                if(quant==0){
+                    byteBuffer.put((byte) ((val>>16)&0xFF));
+                    byteBuffer.put((byte) ((val>>8)&0xFF));
+                    byteBuffer.put((byte) (val&0xFF));
+                }
+                else {
+
+                    byteBuffer.putFloat((((val >> 16) & 0xFF))/255.0f);
+                    byteBuffer.putFloat((((val >> 8) & 0xFF))/255.0f);
+                    byteBuffer.putFloat((((val) & 0xFF))/255.0f);
+                }
+            }
+        }
+        return byteBuffer;
+    }
+}
+
